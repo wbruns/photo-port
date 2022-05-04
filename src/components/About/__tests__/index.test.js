2d763e9ed0b0b4b8830b740e41e94a337321ebe8
wbruns <wbruns07@gmail.com>
2022-05-03 22:31:06 -0500

    wrote tests for About component

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    //renders About tests
+    // first test
+    it('renders', () => {
+        render(<About />);
+    });
+    // second test
+    it('matches snapshot DOM node structure', () => {
+        // render About
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
